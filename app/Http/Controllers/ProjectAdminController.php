021c73b3e77be30a0d8987549422d51e0e3517af
FiodhyAN <73778173+FiodhyAN@users.noreply.github.com>
2023-03-14 22:52:48 +0700

    Fix bug projectadmin dan fix assign reviewer

--- app/Http/Controllers/ProjectAdminController.php
+++ app/Http/Controllers/ProjectAdminController.php
@@ -14,19 +14,36 @@ class ProjectAdminController extends Controller
     public function index()
     {
         $this->authorize('admin');
-        $projects = ProjectUser::with('project')->where('user_id', auth()->user()->id)->get();
+        $projects = ProjectUser::with('project')->where('user_id', auth()->user()->id)->where('user_role', 'admin')->get();
         return view('dashboard.admin.project', compact('projects'));
     }
 
     public function show($id)
     {
         $this->authorize('admin');
-        $users = User::with(['article_user' => function($query) use ($id) {
-            $query->with(['article' => function($query) use ($id) {
-                $query->where('project_id', $id);
-            }]);
-        }])->where('id', '!=', auth()->user()->id)->where('is_superadmin', false)->get();
-        // return $users;
+        
+        $project_limit = Project::where('id', $id)->first();
+        $user_project = ProjectUser::where('project_id', $id)->where('user_role', 'reviewer')->count();
+
+        if ($user_project == $project_limit->limit_reviewer) {
+            $users = User::with(['article_user' => function($query) use ($id) {
+                $query->with(['article' => function($query) use ($id) {
+                    $query->where('project_id', $id);
+                }]);
+            }])->where('id', '!=', auth()->user()->id)->where('is_superadmin', false)->whereHas('article_user', function($query) use ($id) {
+                $query->whereHas('article', function($query) use ($id) {
+                    $query->where('project_id', $id);
+                });
+            })->get();
+        }
+        else {
+            $users = User::with(['article_user' => function($query) use ($id) {
+                $query->with(['article' => function($query) use ($id) {
+                    $query->where('project_id', $id);
+                }]);
+            }])->where('id', '!=', auth()->user()->id)->where('is_superadmin', false)->get();
+        }
+
         $project = ProjectUser::with('project')->where('user_id', auth()->user()->id)->where('project_id', $id)->first();
         return view('dashboard.admin.article.index', [
             'project' => $project,
