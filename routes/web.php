2a77180163d59a0e855e00e3a106aece5bb50be4
fiansyahm <fiansyahmm@gmail.com>
2023-03-24 09:10:20 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ProjectAdminController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AssignReviewerController;
 use App\Http\Controllers\AssessmentController;
+use App\Http\Controllers\DataProcessingController
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
@@ -79,146 +80,9 @@ Route::post('/dashboard/reviewer/assessment/store', [AssessmentController::class
 // sebelumnya bisa ikuti command dibawah
 // cd ./python
 // python run_app_flask.py
-
-use Illuminate\Http\Response;
-
-Route::get('/pengolahan-data', function () {
-    return view('pengolahan_data_slr.home');
-});
-
-
-Route::get('/gambar-graph', function () {
-    $articles = DB::table('graphimage')
-                ->select('base64code')
-                ->get();
-
-    $data = json_decode($articles, true);
-    $response=$data[0]['base64code'];
-    return view('graph', ['src' => "data:image/png;base64, $response"]);
-});
-
-
-Route::get('/my-image', function() {
-    $articles = DB::table('graphimage')
-                ->select('base64code')
-                ->get();
-
-    $data = json_decode($articles, true);
-    $response=$data[0]['base64code'];
-
-    // Create an HTTP response with the image data
-    $headers = [
-        'Content-Type' => 'image/png',
-    ];
-    $statusCode = 200;
-    $content = base64_decode($response);
-    $response = new Response($content, $statusCode, $headers);
-
-    // Return the HTTP response
-    return $response;
-});
-
-
-function getData(){
-    $articles = DB::table('articles')
-                ->select('no', 'keywords', 'abstracts', 'year', 'authors', 'citing_new')
-                ->get();
-
-    $data = json_decode($articles, true);
-    $result = [];
-
-    $flag=0;
-    foreach ($data as $row) {
-        $flag++;
-        if($flag<=0) continue;
-        if($flag>60) break;
-        $keywords = preg_split('/\s*[,;\/]\s*/', $row['keywords']);
-        $authors = preg_split('/\s*[,;\/]\s*/', $row['authors']);
-
-       
-        foreach ($authors as $key => $author) {
-            if (strlen($author) <= 3) {
-                unset($authors[$key]);
-            }
-        }  
-        sort($authors, SORT_NUMERIC);
-          
-        $citingNew = preg_split('/\s*[,;\/]\s*/', $row['citing_new']);
-        foreach ($citingNew as $key => $citing) {
-            if (strlen($citing) <= 1) {
-                unset($citingNew[$key]);
-            }
-        }
-        sort($citingNew, SORT_NUMERIC);
-
-        $abstracts = $keywords;
-
-        if(strlen($row['citing_new'])==1){
-            $result[] = [$row['no'], $keywords, $abstracts,(string) $row['year'],$authors];
-        }         
-        else{
-            $result[] = [$row['no'], $keywords, $abstracts,(string) $row['year'],$authors,  $citingNew];
-        }    
-    }
-    $result[] = ["dummywriter", [], [],[],["dummywriter"]];
-    return $result;
-}
-
-Route::get('/data/rank', function () {
-    $result = getData();
-    // transporse table
-    // https://stackoverflow.com/questions/6297591/how-to-invert-transpose-the-rows-and-columns-of-an-html-table
-    set_time_limit(6000);
-    $response = Http::timeout(6000)->post('http://127.0.0.1:5000/data/rank', [
-        'data' => 
-            $result
-            // [  
-            //     [ "a1", ['a','b','c'],   ['a','b','c','k','l']    ,'1993',['p1','p2']                                              ]
-            //     , [ "a2", ['c','d','e'],   ['a','c','d','e','m','n'],'1993',['p1','p3']                                              ]
-            //     , [ "a3", ['f','g','h'],   ['c','d','f','g','h','o'],'1993',['p2','p4','p5']                                         ]
-            //     , [ "a4", ['i','j'],       ['c','d','p','q']        ,'1994',['p3','p6']      ,['a1','a2']                            ]
-            //     , [ "a5", ['dj','dk'],     ['a','dj','dk','m','r']  ,'1994',['p1','p7']      ,['a1','a2','a3']                       ]
-            //     , [ "a6", ['d','ac','ad'], ['d','ac','ad','s','t']  ,'1994',['p8','p9']      ,['a1','a3']                            ]
-            // ]
-        ,'outer'=>true
-        ,'author-rank'=>10
-    ]);
-    // return $response;
-    // return json_decode($response);
-    $authors = $response[0];
-    $ranks =  $response[1][1];
-
-    // Combine the authors and ranks into an array of arrays
-    $author_ranks = array();
-    for ($i = 0; $i < count($authors); $i++) {
-        $author_ranks[] = array($authors[$i], $ranks[$i]);
-    }
-
-    // Sort the author-rank pairs based on the rank (ascending order)
-    usort($author_ranks, function($a, $b) {
-        return $a[1] - $b[1];
-    });
-
-    return view('pengolahan_data_slr.rank',  ['authors'=> $response[0],'ranktable' => $response[1][0],'rank' => $response[1][1],'author_ranks' => $author_ranks]);
-
-});
-Route::get('/data/graph', function () {
-    $result = getData();
-    set_time_limit(6000);
-    $response =  Http::timeout(6000)->post('http://127.0.0.1:5000/data/graph', [
-        'data' => 
-        $result
-        // [  
-        //     [ "a1", ['a','b','c'],   ['a','b','c','k','l']    ,'1993',['p1','p2']                                              ]
-        //     , [ "a2", ['c','d','e'],   ['a','c','d','e','m','n'],'1993',['p1','p3']                                              ]
-        //     , [ "a3", ['f','g','h'],   ['c','d','f','g','h','o'],'1993',['p2','p4','p5']                                         ]
-        //     , [ "a4", ['i','j'],       ['c','d','p','q']        ,'1994',['p3','p6']      ,['a1','a2']                            ]
-        //     , [ "a5", ['dj','dk'],     ['a','dj','dk','m','r']  ,'1994',['p1','p7']      ,['a1','a2','a3']                       ]
-        //     , [ "a6", ['d','ac','ad'], ['d','ac','ad','s','t']  ,'1994',['p8','p9']      ,['a1','a3']                            ]
-        // ]
-        ,'outer'=>true
-        ,'author-rank'=>10
-    ]);
-    return view('pengolahan_data_slr.graph', ['src' => "data:image/png;base64, $response"]);
-
-});
+Route::post('/pengolahan-data', [DataProcessingController::class, 'pengolahan_data'])->middleware('auth')->name('assessment.store');
+Route::post('/gambar-graph', [DataProcessingController::class, 'gambar_graph'])->middleware('auth')
+Route::post('/gambar-graph', [DataProcessingController::class, 'gambar_graph'])->middleware('auth')
+Route::post('/my-image', [DataProcessingController::class, 'my_image'])->middleware('auth')
+Route::post('/data/rank', [DataProcessingController::class, 'data_rank'])->middleware('auth')
+Route::post('/data/graph', [DataProcessingController::class, 'data_graph'])->middleware('auth')
