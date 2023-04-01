be4941f3cf0fd9f070efcc20e1fa9f845b9c6cfd
fiansyahm <fiansyahmm@gmail.com>
2023-04-02 06:55:54 +0700

    improve to english

--- resources/views/pengolahan_data_slr/metadata.blade.php
+++ resources/views/pengolahan_data_slr/metadata.blade.php
@@ -8,7 +8,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
     
-    <h1 class="text-center mb-5">Hubungan Antar {{$type}} & Keberpengaruhannya</h1>
+    <h1 class="text-center mb-5">{{$type}} relationships and popularity</h1>
     <form class="form-body row g-3" action="/proses-metadata/{{$type}}" method="POST">
         @csrf
         <div class="row">
@@ -22,7 +22,7 @@
             </div>
             <div class="col-md-4">
                 <select class="form-select" aria-label="Default select example" id="top-author" name="top-author">
-                    <option selected>Jumlah {{$type}} Yang Dihighlight</option>
+                    <option disabled selected>-- Select Top {{$type}} --</option>
                     <option value="5">5</option>
                     <option value="10">10</option>
                     <option value="20">20</option>
@@ -30,9 +30,9 @@
             </div>
             <div class="col-md-4">
                 <select class="form-select" aria-label="Default select example" id="outer-author" name="outer-author">
-                    <option selected>Pilihan {{$type}} Tanpa Relasi</option>
-                    <option value="1">Tampilkan</option>
-                    <option value="0">Tidak Tampilkan</option>
+                    <option disabled selected>-- Select {{$type}} display --</option>
+                    <option value="1">Show</option>
+                    <option value="0">Hide</option>
                 </select>
             </div>
         </div>
@@ -40,7 +40,7 @@
     </form>
     <div class="row">
         <div class="col-md-6">
-        <h1 class="text-center mt-5">Graph {{$type}}</h1>
+        <h1 class="text-center mt-5">{{$type}} graph</h1>
             <div class="container text-center">
             <!-- HTML -->
                 <a data-fancybox="gallery" href="{{$src}}">
@@ -57,7 +57,7 @@
             </div>
         </div>
         <div class="col-md-6">
-            <h1 class="text-center mt-5">Tabel Ranking {{$type}}</h1>
+            <h1 class="text-center mt-5">{{$type}} rank table</h1>
             <table class="table">
                 <thead>
                     <tr>
