07e6a8ea7455c8d34cdbe5c7e17a652be6692acd
fiansyahm <fiansyahmm@gmail.com>
2023-06-27 04:13:37 +0700

    Update main.py

--- python/env/main.py
+++ python/env/main.py
@@ -171,5 +171,9 @@ def data(type, name):
             print("Waktu eksekusi program: {:.2f} detik".format(total_time))
             return tmp_dict
 
+@application.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
